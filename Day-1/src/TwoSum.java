6f4754ac5e96ce7a9bfc85b42dd612dce52f1aab
Aryan Jaiswal <aryan19jaiswal@gmail.com>
2026-02-01 19:39:42 +0530

    Day-1 : Two Sum

---
+++ Day-1/src/TwoSum.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum{
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        int[] res = new int[2];
+
+        for(int i=0; i<nums.length; i++)
+        {
+            int comp = target-nums[i];
+            if(map.containsKey(comp))
+            {
+                res[0] = map.get(comp);
+                res[1] = i;
+                break;
+            }
+            map.put(nums[i], i);
+        }
+
+        return res;
+    }
+}
