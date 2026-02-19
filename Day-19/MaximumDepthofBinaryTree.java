e4353bb35a4d1e70c2f6cc86e8562ce93bd08041
Aryan Jaiswal <aryan19jaiswal@gmail.com>
2026-02-19 22:44:15 +0530

    Day-19 : Maximum Depth of A Binary Tree

---
+++ Day-19/MaximumDepthofBinaryTree.java
@@ -0,0 +1,25 @@
+//LEETCODE-104
+public class Solution {
+    public int maxDepth(TreeNode root) {
+        Queue<TreeNode> q = new LinkedList<>();
+        if (root != null) {
+            q.add(root);
+        }
+
+        int level = 0;
+        while (!q.isEmpty()) {
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = q.poll();
+                if (node.left != null) {
+                    q.add(node.left);
+                }
+                if (node.right != null) {
+                    q.add(node.right);
+                }
+            }
+            level++;
+        }
+        return level;
+    }
+}
